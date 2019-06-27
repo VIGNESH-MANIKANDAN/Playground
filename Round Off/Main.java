506dfaaf2423a80abc98c70f0ebb5ae1a35e1ded
VIGNESH-MANIKANDAN <52226090+VIGNESH-MANIKANDAN@users.noreply.github.com>
2019-06-27 15:15:25 +0530

    Test code pushed to repository.

---
+++ Round Off/Main.java
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+  float f;
+  int a;
+   scanf("%f",&f);
+  a=f;
+  printf("%d",a);
+  printf("\n%0.1f",ceil(f));
+  printf("\n%0.1f",floor(f));
+  //fill your code
+}
